42102ec9c4c6f6d78e1acbd6824387b78c1e86a1
kibirevaa <kibirevaa@302D-4>
2021-11-09 12:13:54 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Leaf.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Leaf:IComponent
+    {
+        public int Quantity { get; set; }
+        public int Weight { get; set; }
+        public string Name { get; set; }
+
+        public Leaf(string name,int quantity, int weight)
+        {
+            this.Quantity = quantity;
+            this.Name = name;
+            this.Weight = weight;
+        }
+
+        public void DisplayQuantityAndWeight()
+        {
+            Console.WriteLine(Name + " Quantity: " + Quantity + " Weight: " + Weight);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IComponent enginev6 = new Leaf("Engine V6", 1, 200);
+            IComponent wheel = new Leaf("Wheel", 4, 50);
+            IComponent transmission = new Leaf("Transmission", 1, 60);
+            IComponent differential = new Leaf("Differential", 2, 100);
+            IComponent door = new Leaf("Door", 4, 150);
+            IComponent gastank = new Leaf("Gastank", 1, 170);
+
+            
+            Composite car = new Composite("Car");
+            
+            car.AddComponent(enginev6);
+            car.AddComponent(wheel);
+            car.AddComponent(transmission);
+            car.AddComponent(differential);
+            car.AddComponent(door);
+            car.AddComponent(gastank);
+
+            car.DisplayQuantityAndWeight();
+            
+        }
+    }
+}
